f47d05a0272059b9f0e380be8f83ffa06018d86e
Jim Kang <jimkang@gmail.com>
2021-07-20 21:19:09 -0400

    Stub in grabbing tasks from taskwarrior.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 var { app, BrowserWindow } = require('electron');
 var path = require('path');
+var oknok = require('oknok');
+var { exec } = require('child_process');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -14,6 +16,7 @@ function createWindow() {
 
   win.loadFile('index.html');
 }
+
 app.whenReady().then(() => {
   createWindow();
 });
@@ -28,4 +31,20 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+
+  getTasks();
 });
+
+function getTasks() {
+  exec(
+    'task status:pending export rc.json.array=on',
+    // Unscheduled tasks:
+    //'task scheduled:"" status:pending export rc.json.array=on',
+    oknok({ ok: useTasks, nok: (error) => console.error(error) })
+  );
+}
+
+function useTasks(stdoutString) {
+  var tasks = JSON.parse(stdoutString);
+  console.log(tasks);
+}
